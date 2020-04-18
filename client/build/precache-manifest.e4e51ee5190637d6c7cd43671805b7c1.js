a1b11a22ba5068dda65e7e561b5e251d5ee773c8
Fung <koufongso@gmail.com>
2020-04-18 11:24:45 -0700

    built react app

---
+++ client/build/precache-manifest.e4e51ee5190637d6c7cd43671805b7c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ae912b7448c0876d522",
+    "url": "/static/css/main.ac8f9b31.chunk.css"
+  },
+  {
+    "revision": "1ae912b7448c0876d522",
+    "url": "/static/js/main.4c4807a3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "219f29db5f10382fa35f",
+    "url": "/static/js/2.600514d9.chunk.js"
+  },
+  {
+    "revision": "381f33449f174a2601f3e66c16a9df1f",
+    "url": "/index.html"
+  }
+];
